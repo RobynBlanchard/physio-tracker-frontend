fc3fb02bb34ac8c19ff2f4fab0252fbf8a272e84
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-01-03 15:48:56 +0000

    Fetch server side unless client side routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,36 @@
+// import App from 'next/app';
+// import withApollo from '../util/apollo-client';
+
+// class CustomApp extends App {
+//   render() {
+//     const { Component, pageProps } = this.props;
+
+//     return (
+//       <Component {...pageProps} />
+//     );
+//   }
+// }
+
+// export default withApollo(CustomApp);
+
+// // TODO: userContext - to pass through user id
+
+import React from 'react';
 import App from 'next/app';
-import { withApollo } from '../util/apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+import withData from '../util/apollo-client';
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
-
+    const { Component, pageProps, apollo } = this.props;
     return (
-      <Component {...pageProps} />
+      <ApolloProvider client={apollo}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     );
   }
 }
 
-export default withApollo(MyApp);
-
-// TODO: userContext - to pass through user id
+// Wraps all components in the tree with the data provider
+export default withData(MyApp);
