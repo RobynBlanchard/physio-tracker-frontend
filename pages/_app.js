59b6ba23f82d78d0c525051f7b625e1aafa96441
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-03-25 21:05:43 +0000

    Meta tag in document instead

--- pages/_app.js
+++ pages/_app.js
@@ -65,7 +65,7 @@ class MyApp extends App {
       <>
         <Head>
           <title>Home</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/> */}
 
         </Head>
         <ApolloProvider client={apollo}>
