79b1bd538279226ed128b13b0ad9ce5f41b3c812
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-03-25 21:02:10 +0000

    Try again

--- pages/_app.js
+++ pages/_app.js
@@ -65,6 +65,8 @@ class MyApp extends App {
       <>
         <Head>
           <title>Home</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+
         </Head>
         <ApolloProvider client={apollo}>
           <AuthProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -37,7 +37,10 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
           ></link>
           <link rel="manifest" href="/manifest.json" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/> */}
+          {/* < meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/> */}
+          <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/>
+
         </Head>
         <body>
           <Main />
