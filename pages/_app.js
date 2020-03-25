aa48a5dde57c8dcd88da2a99dfd84dd4f5e69e42
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-03-25 21:07:46 +0000

    Move to _app

--- pages/_app.js
+++ pages/_app.js
@@ -65,8 +65,10 @@ class MyApp extends App {
       <>
         <Head>
           <title>Home</title>
-          {/* <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"/> */}
-
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
+          />
         </Head>
         <ApolloProvider client={apollo}>
           <AuthProvider>
