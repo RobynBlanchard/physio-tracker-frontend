d5b64d27271e2631c50b127e31faab2e7920437a
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-04-06 21:39:13 +0100

    Passing test

--- src/components/SessionPage/test.js
+++ src/components/SessionPage/test.js
@@ -7,7 +7,7 @@ import Sessions, {
   GET_SESSIONS,
   CREATE_SESSION,
   LOADING_MESSAGE,
-  ERROR_MESSAGE
+  ERROR_MESSAGE,
 } from '.';
 import { SessionsList, Button } from '../index';
 
@@ -15,14 +15,14 @@ it('renders without error', async () => {
   const mocks = [
     {
       request: {
-        query: GET_SESSIONS
+        query: GET_SESSIONS,
       },
       result: {
         data: {
-          sessions: []
-        }
-      }
-    }
+          sessions: [],
+        },
+      },
+    },
   ];
 
   await act(async () => {
@@ -51,16 +51,16 @@ it('renders loading state initially', async () => {
 it('renders SessionList when the get Sessions query is successful', async () => {
   const sessionsData = [
     { id: '1', date: '2019-02-03' },
-    { id: '2', date: '2019-02-10' }
+    { id: '2', date: '2019-02-10' },
   ];
 
   const mock = {
     request: {
-      query: GET_SESSIONS
+      query: GET_SESSIONS,
     },
     result: {
-      data: { sessions: sessionsData }
-    }
+      data: { sessions: sessionsData },
+    },
   };
 
   const component = mountWithTheme(
@@ -81,9 +81,9 @@ it('renders SessionList when the get Sessions query is successful', async () => 
 it('displays an error message when the query is unsuccessful', async () => {
   const mock = {
     request: {
-      query: GET_SESSIONS
+      query: GET_SESSIONS,
     },
-    error: new Error('Could not fetch sessions')
+    error: new Error('Could not fetch sessions'),
   };
 
   const component = mountWithTheme(
@@ -101,102 +101,72 @@ it('displays an error message when the query is unsuccessful', async () => {
 
 // adding a session
 describe('adding a session', () => {
-  it('calls create session mutation and displays refetched sessions', async () => {
-    const sessionsData = [
-      { id: '1', date: '2019-02-03' },
-      { id: '2', date: '2019-02-10' }
-    ];
-
-    const mock = {
+  it.only('calls create session mutation and displays refetched sessions', async () => {
+    const mockGetSessions = {
       request: {
-        query: GET_SESSIONS
+        query: GET_SESSIONS,
       },
       result: {
-        data: { sessions: sessionsData }
-      }
+        data: { sessions: [] },
+      },
     };
 
-    const mutationMock = {
+    const mockCreateSession = {
       request: {
         query: CREATE_SESSION,
-        variables: {  date: '2019-02-30' }
+        variables: { data: { date: '2019-02-30' } },
       },
-      result: {
-        data: {
-          __typename: "Mutation",
-          // Mock here the expected result of your query.
-          updateitem: {
-            __typename: "Session",
-            id: '3',
-            date: '2019-02-30'
-            // ...additional data
-          }
-        }
-      }
+      result: { data: { id: '3', date: '2019-02-30' } },
     };
 
-    // const mutationMock = {
-    //   request: {
-    //     query: CREATE_SESSION,
-    //     variables: {
-    //       date: '2019-02-30'
-    //     },
-    //     refetchQueries:[{
-    //       query: GET_SESSIONS,
-    //       //  variables:{
-    //       //   id:4
-    //       //  }
-    //      }],
-    //   },
-    //   // result: {
-    //   //   data: { sessions: sessionsData.concat({id: '3', date: '2019-02-30'}) }
-    //   // }
-    //   result: {
-    //     data: { id: '3', date: '2019-02-30' }
-    //   }
-    // };
-
-    const getMock2 = {
+    const mockRefreshedGetSessions = {
       request: {
-        query: GET_SESSIONS
+        query: GET_SESSIONS,
       },
       result: {
-        data: { sessions: sessionsData.concat({ id: '3', date: '2019-02-30' }) }
-      }
-    }
+        data: { sessions: [{ id: '3', date: '2019-02-30' }] },
+      },
+    };
+
+    const mocks = [
+      mockGetSessions,
+      mockCreateSession,
+      mockRefreshedGetSessions,
+    ];
 
     const component = mountWithTheme(
-      <MockedProvider  mocks={[ mock, mutationMock, getMock2]} addTypename={false}>
+      <MockedProvider mocks={mocks} addTypename={false}>
         <Sessions />
       </MockedProvider>
     );
 
     await updateWrapper(component);
-
-    // console.log(component.debug());
+    console.log('1', component.debug());
 
     component
-      .find('input')
+      .find('#input-new-session-date')
       .simulate('change', { target: { value: '2019-02-30' } });
+      console.log('2', component.debug());
 
     await act(async () => {
+    console.log('3', component.debug());
 
-      // component.find('button').simulate('click');
-      await component.find('button').prop('onClick')();
-
-    console.log('1======');
+      await component.find('button').find('#add-new-session').prop('onClick')();
+    console.log('4',component.debug());
 
     });
-    await updateWrapper(component);
-
-    console.log('2=====');
-
+    // console.log(component.debug());
     console.log(component.debug());
 
-    // expect(component.text()).not.toContain(LOADING_MESSAGE);
-    // expect(component.text()).not.toContain(ERROR_MESSAGE);
+    // expect(component.text()).toContain('Adding session');
+    await updateWrapper(component);
+
+    // console.log(component.debug());
+    // expect(component.text()).toContain('Adding session');
+
+    expect(component.text()).not.toContain(ERROR_MESSAGE);
     const sessionList = component.find(SessionsList);
-    expect(sessionList.length).toEqual(3);
+    expect(sessionList.length).toEqual(1);
     // expect(sessionList.prop('sessions')).toEqual(sessionsData);
   });
 });
