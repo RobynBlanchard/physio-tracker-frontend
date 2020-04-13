2c9bd50432eeab1c7defde1c8632a7b034678c5d
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2020-04-13 21:25:06 +0100

    Remove unused styles

--- src/components/ExercisesList/index.js
+++ src/components/ExercisesList/index.js
@@ -1,5 +1,4 @@
 import {
-  ExerciseListWrapper,
   IconButton,
   StyledIcon,
   ExerciseItemWrapper,
@@ -10,26 +9,24 @@ import { arrayOf, string, shape, func } from 'prop-types';
 const ExercisesList = ({ deleteExercise, exercises = [] }) => {
   return (
     <>
-      <ExerciseListWrapper>
-        {exercises.map((exercise) => {
-          const { name, id } = exercise;
+      {exercises.map((exercise) => {
+        const { name, id } = exercise;
 
-          return (
-            <ExerciseItemWrapper key={`${name}_${id}`}>
-              <TitleLink title={name} exerciseId={id} />
-              <IconButton id="delete-button" onClick={() => deleteExercise(id)}>
-                <StyledIcon
-                  aria-hidden="true"
-                  title="Delete this session?"
-                  aria-label="Delete"
-                  icon="trash-alt"
-                  size="lg"
-                />
-              </IconButton>
-            </ExerciseItemWrapper>
-          );
-        })}
-      </ExerciseListWrapper>
+        return (
+          <ExerciseItemWrapper key={`${name}_${id}`}>
+            <TitleLink title={name} exerciseId={id} />
+            <IconButton id="delete-button" onClick={() => deleteExercise(id)}>
+              <StyledIcon
+                aria-hidden="true"
+                title="Delete this session?"
+                aria-label="Delete"
+                icon="trash-alt"
+                size="lg"
+              />
+            </IconButton>
+          </ExerciseItemWrapper>
+        );
+      })}
     </>
   );
 };
--- src/components/ExercisesList/style.js
+++ src/components/ExercisesList/style.js
@@ -2,19 +2,6 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { BaseButton } from '../../styles/utils';
 
-export const Wrapper = styled.div`
-  max-height: ${({ open }) => (open ? '100px' : '0')};
-  overflow: ${({ open }) => (open ? 'auto' : 'hidden')};
-  border-bottom: 1px solid white;
-  transition: max-height 0.5s, overflow 0.5s 0.5s;
-  color: ${({ theme }) => theme.colors.white};
-`;
-
-export const ExerciseListWrapper = styled.div`
-  max-height: 55vh;
-  overflow-y: scroll;
-`;
-
 export const IconButton = styled(BaseButton)`
   margin: 0 4px;
 `;
@@ -28,4 +15,5 @@ export const StyledIcon = styled(FontAwesomeIcon)`
 export const ExerciseItemWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.white};
 `;
